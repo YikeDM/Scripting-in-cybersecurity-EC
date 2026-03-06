6083aafd578798bc8e9984c79f8f4d24b726569f
YikeDM <118478732+YikeDM@users.noreply.github.com>
2026-03-06 11:55:47 +0000

    Fix echo statements for IPv6 address and hash

--- ipv6_printer.sh
+++ ipv6_printer.sh
@@ -7,9 +7,9 @@ echo $ip > ipv6.file
 hash=$(sha256sum ipv6.file | awk '{print $1}')
 
 echo "***************************************************"
-echo "IPv6 Address = " + $ip
+echo "IPv6 Address = " $ip
 echo ""
-echo "Sha256 Hash = " + $hash
+echo "Sha256 Hash = " $hash
 echo ""
 echo "***************************************************"
 
